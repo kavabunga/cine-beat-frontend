578832dd2945c58d383c661f704b7221a456340f
kavabunga <kavabunga@gmail.com>
2023-03-07 21:30:11 +0200

    feature: add AboutProject component

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -5,7 +5,7 @@ import './Promo.css';
 
 export default function Promo() {
   return (
-    <section className='promo main__section'>
+    <section className='promo'>
       <div className='promo__banner'>
         <h1 className='promo__title'>
           Учебный проект студента факультета Веб&#8209;разработки.
