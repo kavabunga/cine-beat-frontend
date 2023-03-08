1768382e14071b20b464de11bf7ffa25ec5e5100
kavabunga <kavabunga@gmail.com>
2023-03-08 18:44:54 +0200

    feature: add Portfolio component

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,5 +2,44 @@ import React from 'react';
 import './Portfolio.css';
 
 export default function Portfolio() {
-  return <section className='portfolio main__section'></section>;
+  return (
+    <section className='portfolio'>
+      <h2 className='portfolio__title'>Портфолио</h2>
+      <ul className='portfolio__list'>
+        <li className='portfolio__element'>
+          <a
+            href='https://smooth-goldfish.surge.sh/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='portfolio__link app__link'
+          >
+            Статичный сайт
+          </a>
+          <span className='portfolio__icon'>↗</span>
+        </li>
+        <li className='portfolio__element'>
+          <a
+            href='https://hypnotic-toe.surge.sh/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='portfolio__link app__link'
+          >
+            Адаптивный сайт
+          </a>
+          <span className='portfolio__icon'>↗</span>
+        </li>
+        <li className='portfolio__element'>
+          <a
+            href='https://kavabunga.github.io/mesto/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='portfolio__link app__link'
+          >
+            Одностраничное приложение
+          </a>
+          <span className='portfolio__icon'>↗</span>
+        </li>
+      </ul>
+    </section>
+  );
 }
